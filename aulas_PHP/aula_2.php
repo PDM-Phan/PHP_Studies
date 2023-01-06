c62140b8ddabdaee9991a9d0b967534de55bad3b
PDM-Phan <pedroddemelo@gmail.com>
2023-01-06 11:33:42 -0300

    aula2 commit

--- aulas_PHP/aula_2.php
+++ aulas_PHP/aula_2.php
@@ -5,8 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 2</title>
+    <link rel = "stylesheet" href = "styles_aulas/style_aula2.css">   
 </head>
 <body>
-    
+    <u><h1>Cauculadora Simples PHP</h1></u>
+    <section>
+        <form action="" method="post">
+            <br>
+            <label for="number1">Primeiro número:</label>
+            <input type = "number" name = "number1" id = "number1" min = 0 max = 9999></input><br><br>
+            <label for="number2">Segundo número:</label> 
+            <input type = "number" name = "number2" id = "number2" min = 0 max = 9999></input><br><br>
+            <input type="submit" value="+" name="operacao">
+            <input type="submit" value="-" name="operacao">
+            <input type="submit" value="*" name="operacao">
+            <input type="submit" value="/" name="operacao">
+        </form>
+        <br>
+    </section>
+    <section>
+        <?php 
+            if(array_key_exists('operacao', $_POST)) {
+                $n1 = $_POST['number1'];
+                $n2 = $_POST['number2'];
+                $operacao = $_POST['operacao'];
+                caucula($n1, $n2, $operacao);
+            }
+            function caucula($number1, $number2, $op) {
+                if($op == "+") {
+                    echo "resultado => ".($number1 + $number2);
+                }
+                else if($op == "-") {
+                    echo "resultado => ".($number1 - $number2);
+                }
+                else if($op == "*") {
+                    echo "resultado => ".($number1 * $number2);
+                }
+                else {
+                    echo "resultado => ".($number1 / $number2);
+                }
+            }
+        ?>
+    </section>
 </body>
 </html>
