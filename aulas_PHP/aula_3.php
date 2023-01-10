4224b11b14e689243d4875802e083b42196b3410
PDM-Phan <pedroddemelo@gmail.com>
2023-01-10 14:40:50 -0300

    fixed code

--- aulas_PHP/aula_3.php
+++ aulas_PHP/aula_3.php
@@ -2,9 +2,9 @@
 <html lang="pt-br">
 <head>
     <?php 
-        $txt = isset($_POST['t'])?$_POST['t']:"Texto ainda não definido...";
-        $cor = isset($_POST['cor'])?$_POST['cor']:"#000000";
-        $tam = isset($_POST['tam'])?$_POST['tam']:"12px";
+        $txt = isset($_GET['t'])?$_GET['t']:"Texto ainda não definido...";
+        $cor = isset($_GET['cor'])?$_GET['cor']:"#000000";
+        $tam = isset($_GET['tam'])?$_GET['tam']:"12pt";
     ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,25 +13,27 @@
     <link rel="stylesheet" href="styles_aulas/style_aula3.css">
     <style>
         /*Um span da classe texto recendo caracteristicas pré(ou não)definidas*/
-        #echo {
-            font-size = <?php echo $tam; ?>;
-            color = <?php echo $cor; ?>;
+        h3 {
+            font-size: <?php echo $tam; ?>;
+            color: <?php echo $cor; ?>;
+            text-align: center;
+            margin-top: 40pt;
         }
     </style>
 </head>
 <body>
     <div>
-        <form action="" method="post">
+        <form action="" method="get">
             <fieldset><legend><h2 id="legenda">Gerador de Texto Customizavél</h2></legend>
                 <label for="itxt">Texto: </label>
                 <input type="text" name="t" id="itxt"></br>
                 <label for="itam">Tamanho</label>
                 <select name="tam" id="itam"> <!--Select : abre uma caixa para escolha dos valores-->
-                    <option value="8px">8</option>
-                    <option value="10px">10</option>
-                    <option value="14px">14</option>
-                    <option value="20px">20</option>
-                    <option value="40px">40</option>
+                    <option value="8pt">8</option>
+                    <option value="10pt">10</option>
+                    <option value="14pt">14</option>
+                    <option value="20pt">20</option>
+                    <option value="40pt">40</option>
                 </select>
                 <label for="icor">Cor: </label>
                 <input type="color" name="cor" id="icor">
@@ -41,9 +43,11 @@
     </div>
     <div>
         <!--Área onde mostrará o texto-->
+        <h3>
         <?php 
-            echo "<h3 id=echo>$txt</h3>";
+            echo $txt;
         ?>
+        </h3>
     </div>
 </body>
 </html>
