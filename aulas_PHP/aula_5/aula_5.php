2f9acc57c263b4cff8cd726cb88da73253184748
PDM-Phan <pedroddemelo@gmail.com>
2023-01-17 14:34:31 -0300

    aula_5 code completed

--- aulas_PHP/aula_5/aula_5.php
+++ aulas_PHP/aula_5/aula_5.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <script>
+        <?php 
+            $inicio = isset($_POST['inicio'])?$_POST['inicio']:0;
+            $final = isset($_POST['final'])?$_POST['final']:0;
+            $passador = $_POST['passador'];
+        ?>
+    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles_aulas/style_aula5.css">
     <title>Contador Personalizado</title>
 </head>
-<body>
-    
+<body id="paginaC">
+    <section>
+        <?php 
+            if ($inicio > $final) {
+                $passador *= -1;
+                for ($inicio; $inicio>=$final; $inicio += $passador) {
+                    echo " $inicio ";
+                }
+            } else {
+                for ($inicio; $inicio<=$final; $inicio += $passador) {
+                    echo " $inicio ";
+                }
+            }
+            
+        ?>
+    </section>
+    <br/><a href="aula_5.html">Voltar</a>
 </body>
 </html>
