eb5f8e74a86e7db840d6eb64c8d552d560265022
PDM-Phan <pedroddemelo@gmail.com>
2023-01-23 10:18:16 -0300

    ...

--- aulas_PHP/aula_6/aula_6_2.php
+++ aulas_PHP/aula_6/aula_6_2.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Número Primo?</title>
+    <title>Resultado</title>
     <link rel="stylesheet" href="../styles_aulas/style_aula6.css">
 </head>
 <body>
