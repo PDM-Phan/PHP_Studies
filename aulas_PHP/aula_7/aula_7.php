6ab24d6219a9255fcc9dd02b3774997efcd9bf44
PDM-Phan <pedroddemelo@gmail.com>
2023-01-26 14:00:55 -0300

    ...

--- aulas_PHP/aula_7/aula_7.php
+++ aulas_PHP/aula_7/aula_7.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    
-    
-    <?php
-        function gerador_inputs($num_inputs) {
-            for ($i=0; $i<$num_inputs; $i++) {
-                
-            }
-        }
-    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,6 +8,6 @@
     <title>Aula 7</title>
 </head>
 <body>
-    <h1>Praticando paramêtros dinâmicos...</h1>
+    
 </body>
 </html>
