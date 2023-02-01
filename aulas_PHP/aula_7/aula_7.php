9dc8c92c0ffa95f2d79caf63660fa4270b512561
PDM-Phan <pedroddemelo@gmail.com>
2023-02-01 10:48:58 -0300

    creation of the final steps

--- aulas_PHP/aula_7/aula_7.php
+++ aulas_PHP/aula_7/aula_7.php
@@ -2,25 +2,58 @@
 <html lang="pt-br">
 <head>
     <?php
-        $num1 = isset($_POST['num1'])?$_POST['num1']:0;
-        $num2 = isset($_POST['num2'])?$_POST['num2']:0;
-        $num3 = isset($_POST['num3'])?$_POST['num3']:0;
-        $num4 = isset($_POST['num4'])?$_POST['num4']:0;
-        $num5 = isset($_POST['num5'])?$_POST['num5']:0;
-        $acao = $_POST['acoes'];
-        function cauculadora($number1, $number2, $number3, $number4, $number5, $acaos) {
-            $itens = func_get_args();
+        function calculadora($number1, $number2, $number3, $number4, $number5, $acaos) {
+            $itens = func_get_args(); 
             $t_itens = func_num_args();
-            if ($acaos=="+") {
-                $res = 0;
-                for ($i=0; $i<$t_itens; $i++) {
-                    if (is_numeric($itens[$i])) {
-                        $res += $itens[$i];
+            $loop = 1;
+            $num1 = $itens[0];
+            $num2 = $itens[1];
+            $res = 0;
+            switch ($acaos) {
+                case "+":
+                    while (is_numeric($itens[$loop])) {
+                        $loop++;
+                        $res = $num1 + $num2;
+                        $num1 = $res;
+                        $num2 = $itens[$loop];
                     }
-                }
+                    break;
+                case "-":
+                    while (is_numeric($itens[$loop])) {
+                        $loop++;
+                        $res = $num1 - $num2;
+                        $num1 = $res;
+                        $num2 = $itens[$loop];
+                    }
+                    break;
+                case "*":
+                    while (is_numeric($itens[$loop])) {
+                        $loop++;
+                        $res = $num1 * $num2;
+                        $num1 = $res;
+                        $num2 = $itens[$loop];
+                    }
+                    break;
+                case "/":
+                    while (is_numeric($itens[$loop])) {
+                        $loop++;
+                        $res = $num1 / $num2;
+                        $num1 = $res;
+                        $num2 = $itens[$loop];
+                    }
+                    break;
+                default:
+                    while (is_numeric($itens[$loop])) {
+                        $loop++;
+                        $res = $num1 + $num2;
+                        $num1 = $res;
+                        $num2 = $itens[$loop];
+                    }
+                    $res /= $loop;
             }
-            echo "Resultado da operação é $res.";
+            echo "<span>Resultado da operação é $res.</span>";
         }
+
     ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,7 +64,14 @@
 <body>
     <div>
         <?php
-            cauculadora($num1, $num2, $num3, $num4, $num5, $acao);
+            $num1 = isset($_POST['num1'])?$_POST['num1']:0;
+            $num2 = isset($_POST['num2'])?$_POST['num2']:0;
+            $num3 = isset($_POST['num3'])?$_POST['num3']:0;
+            $num4 = isset($_POST['num4'])?$_POST['num4']:0;
+            $num5 = isset($_POST['num5'])?$_POST['num5']:0;
+            $acao = $_POST['acoes'];
+        
+            calculadora($num1, $num2, $num3, $num4, $num5, $acao);
         ?>
     </div>
     <div><a href="aula_7.html" class="botao">Voltar</a></div>
