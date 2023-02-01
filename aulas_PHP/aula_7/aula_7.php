1e154c41ec559784c7ffd9af850ffb0494600358
PDM-Phan <pedroddemelo@gmail.com>
2023-02-01 14:39:43 -0300

    Function fixes

--- aulas_PHP/aula_7/aula_7.php
+++ aulas_PHP/aula_7/aula_7.php
@@ -3,51 +3,62 @@
 <head>
     <?php
         function calculadora($number1, $number2, $number3, $number4, $number5, $acaos) {
-            $itens = func_get_args(); 
+            $itens = func_get_args(); // Recebe todos os valores passados na pagina anterior
             $t_itens = func_num_args();
+            $new_array; // irá receber apenas aqueles valores que possuem números
+            //var_dump($itens);  -> verificar quais os valores totais de itens
+
+            for ($i=0; $i<$t_itens; $i++) { // Loop para criar a nova lista que irá receber apenas as que tiverem inputs
+                if ($itens[$i] === "") {
+                    unset($itens[$i]);
+                }
+            }
+            $new_array = array_values($itens);// Reorganizar a lista criada, com os index certos
+
+            //var_dump($new_array);  -> verificar quais os valores da nova lista
             $loop = 1;
-            $num1 = $itens[0];
-            $num2 = $itens[1];
+            $num1 = $new_array[0];
+            $num2 = $new_array[1];
             $res = 0;
             switch ($acaos) {
+                // Calculo das operações utilizando o algoritimo de fibonacci, apenas com algumas modificações necessárias para a situação
                 case "+":
-                    while (is_numeric($itens[$loop])) {
+                    while (is_numeric($new_array[$loop])){
                         $loop++;
                         $res = $num1 + $num2;
                         $num1 = $res;
-                        $num2 = $itens[$loop];
+                        $num2 = $new_array[$loop];
                     }
                     break;
                 case "-":
-                    while (is_numeric($itens[$loop])) {
+                    while (is_numeric($new_array[$loop])) {
                         $loop++;
                         $res = $num1 - $num2;
                         $num1 = $res;
-                        $num2 = $itens[$loop];
+                        $num2 = $new_array[$loop];
                     }
                     break;
                 case "*":
-                    while (is_numeric($itens[$loop])) {
+                    while (is_numeric($new_array[$loop])) {
                         $loop++;
                         $res = $num1 * $num2;
                         $num1 = $res;
-                        $num2 = $itens[$loop];
+                        $num2 = $new_array[$loop];
                     }
                     break;
                 case "/":
-                    while (is_numeric($itens[$loop])) {
+                    while (is_numeric($new_array[$loop])) {
                         $loop++;
                         $res = $num1 / $num2;
                         $num1 = $res;
-                        $num2 = $itens[$loop];
+                        $num2 = $new_array[$loop];
                     }
                     break;
                 default:
-                    while (is_numeric($itens[$loop])) {
-                        $loop++;
+                    while (is_numeric($new_array[$loop])){
                         $res = $num1 + $num2;
                         $num1 = $res;
-                        $num2 = $itens[$loop];
+                        $num2 = $new_array[$loop];
                     }
                     $res /= $loop;
             }
