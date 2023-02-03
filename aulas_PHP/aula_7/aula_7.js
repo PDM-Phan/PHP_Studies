b7543639c405e10452314fb23bfc1dbe6be3a874
PDM-Phan <pedroddemelo@gmail.com>
2023-02-03 09:32:58 -0300

    validation completed

--- aulas_PHP/aula_7/aula_7.js
+++ aulas_PHP/aula_7/aula_7.js
@@ -7,19 +7,17 @@ function verificar() {
         window.document.getElementById("num5")
     ];
 
-    let v_valido = 0;
-
-    console.log(num);
+    let v_valido = 0; // Verificando quantos valores possuem entradas
 
     for (let i=0; i<num.length; i++) {
-        if (num[i].value.length > 0) {
+        if (num[i].value.length > 0) { // Se o valor não for vazio, ele adiciona na variavel v_valido
             v_valido++;
         }
     }
 
     if (v_valido < 2) {
-        window.alert("[ERRO] Preencha pelo menos 2 campos.");
-        window.location.href = "http://localhost/PHP_Studies/aulas_PHP/aula_7/aula_7.html";
-        return false;
+        window.alert("[ERRO] Preencha pelo menos 2 campos."); // Levanta um aviso para preencher no minimo 2 campos
+        window.location.href = "http://localhost/PHP_Studies/aulas_PHP/aula_7/aula_7.html"; // mantem a pagina na mesma
+        return false; // impede o submit de ser enviado
     }
 }
